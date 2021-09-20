a5ac5e240d34fdcff37300a627362d8ec7e25a82
Overcomzi <ilya_zelentsov23@inbox.ru>
2021-09-20 18:12:06 +0300

    Добафил функции по добавлению смс в БД

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,16 @@
+package com.example.sweater;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
